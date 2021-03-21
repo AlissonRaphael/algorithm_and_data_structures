c924407a09759b0e4547b89e17917b54386bb2bf
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-21 16:42:54 -0300

    add search function

--- 01_vetor_não_ordenado.py
+++ 01_vetor_não_ordenado.py
@@ -16,20 +16,30 @@ class VetorNaoOrdenado:
 
   # BigO => O(1)
   def insere(self, valor):
-    if self.ultima_posicao == self.capacidade - 1:
+    if self.ultima_posicao == self.capacidade - 1: 
       print('Capacidade máxima atingida.')
     else:
       self.ultima_posicao += 1
       self.valores[self.ultima_posicao] = valor
+
+  # BigO => O(n)
+  def pesquisar(self, valor):
+    for i in range(self.ultima_posicao + 1):
+      if valor == self.valores[i]:
+        return i
+    return -1
 
 
 vetor = VetorNaoOrdenado(5)
 
 vetor.insere(2)
 vetor.insere(3)
-vetor.insere(5)
 vetor.insere(8)
+vetor.insere(9)
 vetor.insere(1)
 vetor.imprime()
 
 vetor.insere(4)
+print(vetor.pesquisar(8))
+print(vetor.pesquisar(11)) # pior caso: não existe no vetor.
+print(vetor.pesquisar(2)) # melhor caso: na primeira posição.
