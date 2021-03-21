b96bc607dfb3f35ec9b8b4fdae4c8cf95f77445d
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-20 22:11:21 -0300

    add insertion function

--- 01_vetor_não_ordenado.py
+++ 01_vetor_não_ordenado.py
@@ -14,6 +14,22 @@ class VetorNaoOrdenado:
       for i in range(self.ultima_posicao + 1):
         print(i, '-', self.valores[i])
 
+  # BigO => O(1)
+  def insere(self, valor):
+    if self.ultima_posicao == self.capacidade - 1:
+      print('Capacidade máxima atingida.')
+    else:
+      self.ultima_posicao += 1
+      self.valores[self.ultima_posicao] = valor
+
 
 vetor = VetorNaoOrdenado(5)
+
+vetor.insere(2)
+vetor.insere(3)
+vetor.insere(5)
+vetor.insere(8)
+vetor.insere(1)
 vetor.imprime()
+
+vetor.insere(4)
