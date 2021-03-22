99e93435e760dc75d4cfebb72a57e11bfed2d31c
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-22 14:59:05 -0300

    rename file 02_vetor_ordenado

---
+++ 02_vetor_ordenado_pesquisa_linear.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+class VetorNaoOrdenado:
+  def __init__(self, capacidade):
+    self.capacidade = capacidade
+    self.ultima_posicao = -1
+    self.valores = np.empty(self.capacidade, dtype=int)
+
+  # BigO => O(n)
+  def imprime(self):
+    if self.ultima_posicao == -1:
+      print('O vetor está vazio.')
+    else:
+      for i in range(self.ultima_posicao + 1):
+        print(i, '-', self.valores[i])
+
+  # BigO => O(n)
+  def insere(self, valor):
+    if self.ultima_posicao == self.capacidade - 1:
+      print('Capacidade máxima atingida.')
+      return
+    
+    posicao = 0
+    for i in range(self.ultima_posicao + 1):
+      posicao = i
+      if self.valores[i] > valor:
+        break
+      elif i == self.ultima_posicao:
+        posicao = i + 1
+      
+    x = self.ultima_posicao
+    while x >= posicao:
+      self.valores[x+1] = self.valores[x]
+      x -= 1
+
+    self.valores[posicao] = valor
+    self.ultima_posicao += 1
+
+  # BigO => O(n)
+  def pesquisar(self, valor):
+    for i in range(self.ultima_posicao + 1):
+      if self.valores[i] > valor:
+        return -1
+      
+      if self.valores[i] == valor:
+        return i
+
+      if i == self.ultima_posicao:
+        return -1
+
+  # BigO => O(n)
+  def excluir(self, valor):
+    posicao = self.pesquisar(valor)
+    if posicao == -1:
+      return -1
+    else:
+      for i in range(posicao, self.ultima_posicao):
+        self.valores[i] = self.valores[i+1]
+      
+      self.ultima_posicao -= 1
+
+
+vetor = VetorNaoOrdenado(5)
+
+vetor.insere(2)
+vetor.insere(3)
+vetor.insere(8)
+vetor.insere(9)
+vetor.insere(1)
+vetor.imprime()
+
+print(vetor.pesquisar(9))
+print(vetor.pesquisar(11)) # pior caso: não existe no vetor.
+print(vetor.pesquisar(1)) # # melhor caso: na primeira posição.
+
+vetor.excluir(3)
+vetor.imprime()
+print(vetor.excluir(13))
