8127a7adcd60cc3216e7230bfdb7cdd66694dc24
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-24 00:33:30 -0300

    create 09_listas_encadeadas_simples.py file

---
+++ 09_listas_encadeadas_simples.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class No:
+  def __init__(self, valor):
+    self.valor = valor
+    self.proximo = None
+  
+
+
+class ListaEncadeada:
+  def __init__(self):
+    self.primeiro = None
+  
