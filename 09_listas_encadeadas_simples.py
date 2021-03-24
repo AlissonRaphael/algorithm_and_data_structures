351132720809e59295836a29a86d3a49f69eb6e7
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-24 01:13:01 -0300

    add insert and show functions

--- 09_listas_encadeadas_simples.py
+++ 09_listas_encadeadas_simples.py
@@ -4,6 +4,9 @@ class No:
   def __init__(self, valor):
     self.valor = valor
     self.proximo = None
+
+  def mostra_no(self):
+    print(self.valor)
   
 
 
@@ -11,3 +14,13 @@ class ListaEncadeada:
   def __init__(self):
     self.primeiro = None
   
+  def insere_inicio(self, valor):
+    novo = No(valor)
+    novo.proximo = self.primeiro
+    self.primeiro = novo
+
+  def mostrar(self):
+    atual = self.primeiro
+    while atual != None:
+      atual.mostra_no()
+      atual = atual.proximo
