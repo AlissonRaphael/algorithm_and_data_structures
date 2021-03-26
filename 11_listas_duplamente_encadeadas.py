907464651516d50081d64c4603132bfaad56d692
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-26 16:17:53 -0300

    test/refact and add show insert at the beginning function

--- 11_listas_duplamente_encadeadas.py
+++ 11_listas_duplamente_encadeadas.py
@@ -7,13 +7,13 @@ class No:
     self.proximo = None
     self.anterior = None
 
-  def mostra_no(self):
+  def mostrar_no(self):
     print(self.valor)
 
 
 class ListaDuplamenteEncadeada:
 
-  def __init__(self, valor):
+  def __init__(self):
     self.primeiro = None
     self.ultimo = None
 
@@ -30,10 +30,19 @@ class ListaDuplamenteEncadeada:
     novo.proximo = self.primeiro
     self.primeiro = novo
 
+  def insere_final(self, valor):
+    novo = No(valor)
+    if self.__lista_vazia():
+      self.primeiro = novo
+    else:
+      self.ultimo.proximo = novo
+      novo.anterior = self.ultimo
+    self.ultimo = novo
+
   def mostrar_frente(self):
     atual = self.primeiro
     while atual != None:
-      atual.mostra_no()
+      atual.mostrar_no()
       atual = atual.proximo
 
   def mostrar_traz(self):
@@ -41,3 +50,13 @@ class ListaDuplamenteEncadeada:
     while atual != None:
       atual.mostrar_no()
       atual = atual.anterior
+
+
+lista = ListaDuplamenteEncadeada()
+lista.insere_inicio(1)
+lista.insere_final(2)
+lista.insere_final(3)
+lista.insere_inicio(0)
+lista.insere_final(4)
+lista.mostrar_frente()
+lista.mostrar_traz()
