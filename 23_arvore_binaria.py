e5f5fa632531c597e999faa9a9c93b073d35cb42
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-28 01:07:34 -0300

    add insert function

--- 23_arvore_binaria.py
+++ 23_arvore_binaria.py
@@ -12,3 +12,43 @@ class ArvoreBinariaBusca:
   def __init__(self):
     self.raiz = None
 
+  def inserir(self, valor):
+    novo = No(valor)
+
+    # se a árvore estiver vazia
+    if self.raiz == None:
+      self.raiz = novo
+    else:
+      atual = self.raiz
+
+      while True:
+        pai = atual
+
+        if valor < atual.valor:
+          atual = atual.esquerda
+          if atual == None:
+            pai.esquerda = novo
+            return
+        else:
+          atual = atual.direita
+          if atual == None:
+            pai.direita = novo
+            return
+
+
+arvore = ArvoreBinariaBusca()
+arvore.inserir(53)
+arvore.inserir(30)
+arvore.inserir(14)
+arvore.inserir(39)
+arvore.inserir(9)
+arvore.inserir(23)
+arvore.inserir(34)
+arvore.inserir(49)
+arvore.inserir(72)
+arvore.inserir(61)
+arvore.inserir(84)
+arvore.inserir(79)
+
+print(arvore.raiz.esquerda.valor)
+print(arvore.raiz.direita.valor)
