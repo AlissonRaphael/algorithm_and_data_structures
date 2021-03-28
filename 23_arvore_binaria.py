3bb7fb7bff0b30998d6c8ecade82ab07be4fd9c6
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-28 00:56:12 -0300

    create 23_arvore_binaria.py file

---
+++ 23_arvore_binaria.py
@@ -0,0 +1,14 @@
+class No:
+  def __init__(self, valor):
+    self.valor = valor
+    self.esquerda = None
+    self.direita = None
+  
+  def mostra_no(self):
+    print(self.valor)
+
+
+class ArvoreBinariaBusca:
+  def __init__(self):
+    self.raiz = None
+
