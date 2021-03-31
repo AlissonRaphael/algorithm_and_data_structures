00e38687c31cfcc82ec14ca8ce4d437140873525
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-03-30 23:53:36 -0300

    refact 23_arvore_binaria.py file

--- 23_arvore_binaria.py
+++ 23_arvore_binaria.py
@@ -38,9 +38,6 @@ class ArvoreBinariaBusca:
             self.ligacoes.append(f'  {pai.valor} -> {novo.valor}')
             return
 
-  def pesquisa(self, valor):
-    #function search
-
 
 arvore = ArvoreBinariaBusca()
 arvore.inserir(53)
